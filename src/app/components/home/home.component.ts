6c53f2b23494b8aa580529c8fb31e3d6a61b89bb
PriNcee11 <princee11@hotmail.com>
2017-11-07 00:05:42 +0100

    updated cli

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 import { DataService } from "../../services/DataService.service";
 
@@ -14,18 +15,26 @@ export class HomeComponent implements OnInit{
     pageTitle: string;
     viewMoreLabel: string;
 
-    constructor( private _dataService: DataService ) {}
+    constructor( private _dataService: DataService, private _sanitizer: DomSanitizer ) {}
 
     ngOnInit() {
         //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
         //Add 'implements OnInit' to the class.
         this._dataService.getData().subscribe( data => {
-            this.items = data.items;
+            this.items = this.sanitizeUrls(data.items);
             this.pageTitle = data.pageTitle;
             this.viewMoreLabel = data.viewMoreLabel;
-        });
+        });        
+    }
 
-        
+    sanitizeUrls(items){
+        for(let i in items){
+            // set default image if player does not have it
+            var url = items[i]['imageURL'] ? items[i]['imageURL'] : 'assets/images/fake-image-player-V.jpg';
+            items[i]['imageURL'] = this._sanitizer.bypassSecurityTrustStyle(`url(${url})`);
+        }
+
+        return items;
     }
 
 
