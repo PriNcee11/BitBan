eada2cd291de5b397fff4596987f812d1c99cd5a
PriNcee11 <princee11@hotmail.com>
2017-11-07 15:02:19 +0100

    list component, filter pipe added, style

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,23 +11,51 @@ import { DataService } from "../../services/DataService.service";
 export class HomeComponent implements OnInit{
 
     items: any[];
+    serie: any[] = [];
+    serieAux: any[] = [];
     pageTitle: string;
     viewMoreLabel: string;
+    filter: string = "";
 
     constructor( private _dataService: DataService ) {}
 
     ngOnInit() {
-        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-        //Add 'implements OnInit' to the class.
+        // Get the json data from the service
         this._dataService.getData().subscribe( data => {
             this.items = data.items;
             this.pageTitle = data.pageTitle;
             this.viewMoreLabel = data.viewMoreLabel;
+
+            // Make an array that contains the unique first letter of all the objects
+            this.checkValues();
         });
 
+        // Create an array with the filter options
+        this.fillSerie();
+
+        // Set a default filter
+        this.setFilter(this.filter);
         
     }
 
+    private fillSerie(){
+        this.serie.push('0-9');
 
+        for(let i=65; i<=90; i++){
+            this.serie.push(String.fromCharCode(i));
+        }
+    }
+
+    private checkValues(){
+        this.items.forEach(element => {
+            if(element.nick != undefined){
+                this.serieAux[element.nick.charAt(0).toUpperCase()] = element.nick.charAt(0).toUpperCase();                
+            }
+        });
+    }
+
+    setFilter(filter: string){
+        this.filter = filter;
+    }
 
 }
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -11,14 +11,12 @@ import { DataService } from "../../services/DataService.service";
 export class ListComponent implements OnInit{
 
     items: any;
-
     serie: any[] = [];
+    filter: string = "*";
 
     constructor( private _dataService: DataService ) {}
 
     ngOnInit() {
-        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-        //Add 'implements OnInit' to the class.
         this._dataService.getData().subscribe( data => this.items = data );
 
         this.fillSerie();
@@ -26,10 +24,16 @@ export class ListComponent implements OnInit{
 
     private fillSerie(){
         this.serie.push('0-9');
+
         for(let i=65; i<=90; i++){
             this.serie.push(String.fromCharCode(i));
         }
-        console.log(this.serie);
+    }
+
+    setFilter(filter: string){
+        this.filter = filter;
+        sessionStorage.setItem("filter", filter);
+        console.log(sessionStorage.getItem("filter"));
     }
 
 }
